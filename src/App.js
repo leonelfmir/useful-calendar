b3f68640591adb1b4133e4ade50cd16747216ef8
leo <leonelfmir@hotmail.com>
2021-10-16 11:50:21 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+// @flow
+import "./App.css";
+import LayOut from "./src/LayOut";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(): any {
+  return <LayOut />;
 }
 
 export default App;
---
+++ src/src/LayOut.js
@@ -0,0 +1,7 @@
+// @flow
+
+import React from "react";
+
+export default function LayOut(): React$Element<"div"> {
+  return <div>Here</div>;
+}
