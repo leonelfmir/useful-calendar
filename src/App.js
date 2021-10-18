d6c7c771b24c30736a7025d31131e19e787ff7ef
leo <leonelfmir@hotmail.com>
2021-10-18 09:28:04 -0400

    getting data

--- src/App.js
+++ src/App.js
@@ -1,34 +1,38 @@
 // @flow
 import "./App.css";
 import LayOut from "./src/LayOut";
-import React from "react";
+import * as React from "react";
+import { useEffect, useState } from "react";
 import Year from "./src/Year";
 import type { DaysToMark } from "./src/Year";
 
 function App(): any {
+  const [data, setData] = useState<?InputData>(null);
+  useEffect(() => {
+    getData().then((yearsData) => {
+      const d = yearsData.map((year) => {
+        return {
+          ...year,
+          dates: year.dates.map((date) => {
+            const da = Date.parse(date.date);
+            const dd = { ...date, date: da };
+          }),
+        };
+      });
+      setData(d);
+    });
+  }, [setData]);
+
+  if (data == null) {
+    return null;
+  }
+
   const year = 2021;
-  const holidays: DaysToMark = [
-    {
-      date: new Date(2021, 10, 25),
-      description: "Thanksgiving",
-    },
-    {
-      date: new Date(2021, 10, 26),
-      description: "After Thanksgiving",
-    },
-    {
-      date: new Date(2021, 11, 24),
-      description: "Other",
-    },
-    {
-      date: new Date(2021, 11, 27),
-      description: "Other",
-    },
-    {
-      date: new Date(2021, 11, 31),
-      description: "Other",
-    },
-  ];
+  const holidays = data.find((d) => d.year === year)?.dates;
+  if (holidays == null) {
+    return null;
+  }
+
   return (
     <LayOut>
       <Year year={year} daysToMark={holidays} />
@@ -36,4 +40,18 @@ function App(): any {
   );
 }
 
+async function getData() {
+  const data = await fetch("holidays.json", {
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+  });
+  return data.json();
+}
 export default App;
+
+type InputData = $ReadOnlyArray<{
+  year: number,
+  dates: DaysToMark,
+}>;
